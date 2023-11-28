aa655b7c23b0a04f8fa7910370bcdd1ad4d43876
DenRassk <112314783+DenRassk@users.noreply.github.com>
2023-11-29 01:53:47 +0700

    добавил ДЗ к 3 семинару

---
+++ dz3/dz3/src/Main.java
@@ -0,0 +1,17 @@
+package dz3;
+
+import dz3.Number.NumberCheck;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(NumberCheck.evenOddNumber(10));
+        System.out.println(NumberCheck.evenOddNumber(15));
+        System.out.println(NumberCheck.numberInInterval(23));
+        System.out.println(NumberCheck.numberInInterval(33));
+        System.out.println(NumberCheck.numberInInterval(101));
+
+
+
+    }
+}
